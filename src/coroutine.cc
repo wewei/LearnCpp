f364ffcae19a5d870c061915b2f261d55e4e73e6
Scott Wei <leescott@microsoft.com>
2023-09-20 17:46:40 +0800

    Implement the Task monadic methods

--- src/coroutine.cc
+++ src/coroutine.cc
@@ -10,6 +10,7 @@ struct ReturnObject {
     std::suspend_never initial_suspend() { return {}; }
     std::suspend_never final_suspend() noexcept { return {}; }
     void unhandled_exception() {}
+    void return_void() {}
   };
 };
 
--- src/tasks.cc
+++ src/tasks.cc
@@ -3,21 +3,22 @@
 #include <iostream>
 #include <vector>
 #include <type_traits>
+#include <variant>
 #include "./tasks.h"
 
 template <typename T>
 using Handler = std::function<void(T)>;
 
-template <typename Val, typename Err>
-using Task = std::function<void(Handler<const Val &>, Handler<const Err &>)>;
+template <typename Err, typename Val>
+using Task = std::function<void(Handler<Err>, Handler<Val>)>;
 
 template <typename T>
 using Decorator = std::function<T(T)>;
 
-template <typename Val, typename Err>
-Task<Val, Err> either(Task<Val, Err> task1, Task<Val, Err> task2) {
-    return [=](Handler<const Val &> resolve, Handler<const Err &> reject) {
-        task1(resolve, [=](Err _) { task2(resolve, reject); });
+template <typename Err, typename Val>
+Task<Err, Val> either(Task<Err, Val> task1, Task<Err, Val> task2) {
+    return [=](Handler<const Err &> reject, Handler<Val> resolve) {
+        task1([=](Err _) { task2(reject, resolve); }, resolve);
     };
 }
 
@@ -25,27 +26,72 @@ struct TimeoutRunner {
     virtual void timeout(int ms, std::function<void()> callback) = 0;
 };
 
-template <typename Val, typename Err>
-Task<Val, Err> withDelay(TimeoutRunner &runner, int ms, Task<Val, Err> task) {
-    return [=, &runner](Handler<const Val &> resolve, Handler<const Err &> reject) {
-        runner.timeout(ms, [=]() { task(resolve, reject); });
+template <typename Err, typename Val>
+Task<Err, Val> withDelay(TimeoutRunner &runner, int ms, Task<Err, Val> task) {
+    return [=, &runner](Handler<Err> reject, Handler<Val> resolve) {
+        runner.timeout(ms, [=]() { task(reject, resolve); });
     };
 }
 
-template <typename Val, typename Err>
-Task<Val, Err> withRetry(TimeoutRunner &runner, int ms, Task<Val, Err> task1, Task<Val, Err> task2) {
+template <typename Err, typename Val>
+Task<Err, Val> withRetry(TimeoutRunner &runner, int ms, Task<Err, Val> task1, Task<Err, Val> task2) {
     return either(task1, withDelay(runner, ms, task2));
 }
 
-template <typename Val, typename Err>
-Task<Val, Err> withFinally(std::function<void()> callback, Task<Val, Err> task) {
-    return [=](Handler<const Val &> resolve, Handler<const Err &> reject) {
+template <typename Err, typename Val>
+Task<Err, Val> withFinally(std::function<void()> callback, Task<Err, Val> task) {
+    return [=](Handler<Err> reject, Handler<Val> resolve) {
         task(
-            [=](Val val) { resolve(val); callback(); },
-            [=](Err err) { reject(err); callback(); }
+            [=](Err err) { reject(err); callback(); },
+            [=](Val val) { resolve(val); callback(); }
         );
     };
 }
+
+template <typename Err, typename Val>
+Task<Err, Val> resolveWith(Val val) {
+    return [=](Handler<Err> _, Handler<Val> resolve) { resolve(val); };
+}
+
+template <typename Err, typename Val>
+Task<Err, Val> rejectWith(Err err) {
+    return [=](Handler<Err> reject, Handler<Val> _) {
+        reject(err);
+    };
+}
+
+
+template <typename Err, typename Val>
+Task<Err, Val> pure(Val val) {
+    return resolveWith<Err, Val>(val);
+}
+
+template <typename Err, typename ValF, typename ValT>
+Task<Err, ValT> operator >> (Task<Err, ValF> taskF, std::function<Task<Err, ValT>(ValF)> f) {
+    return [=](Handler<Err> reject, Handler<ValT> resolve) {
+        taskF(reject, [=](ValF valF) {
+            f(valF)(reject, resolve);
+        });
+    };
+}
+
+template <typename Err, typename Val>
+std::function<Task<Err, Val>(Val)> delay(TimeoutRunner &runner, int ms) {
+    return [=, &runner](Val value) {
+        return [=, &runner](auto _, auto resolve) {
+            runner.timeout(ms, [=]() { resolve(value); });
+        };
+    };
+}
+
+template <typename Err, typename Val>
+std::function<Task<Err, Val>(Val)> tap(std::function<void(Val)> callback) {
+    return [=](Val val) {
+        callback(val);
+        return pure<Err, Val>(val);
+    };
+}
+
 
 // template <typename Ret, typename ...Args>
 // Decorator<std::function<Ret(Args...)>> prependBehavior(std::function<void(Args...)> callback) {
@@ -97,16 +143,6 @@ Task<Val, Err> withFinally(std::function<void()> callback, Task<Val, Err> task) 
 // }
 
 // Test utilities
-template <typename Val, typename Err>
-Task<Val, Err> resolveWith(Val val) {
-    return [=](Handler<const Val &> resolve, Handler<const Err &> _) { resolve(val); };
-}
-
-template <typename Val, typename Err>
-Task<Val, Err> rejectWith(Err err) {
-    return [=](Handler<const Val &> _, Handler<const Err &> reject) { reject(err); };
-}
-
 template <typename Arg, typename ...Args>
 void threadLog(Arg&& arg, Args&&... args) {
     std::cout << "[Thread " << std::this_thread::get_id() << "]: ";
@@ -119,16 +155,9 @@ class ThreadRunner : public TimeoutRunner {
 private:
     std::vector<std::thread> threads;
 public:
-    ~ThreadRunner() {
-        for (std::thread &thread: threads) {
-            threadLog("Joining thread", thread.get_id());
-            thread.join();
-        }
-    }
     virtual void timeout(int ms, std::function<void()> callback) override;
+    void join();
 };
-
-ThreadRunner globalRunner;
 
 void ThreadRunner::timeout(int ms, std::function<void()> callback) {
     threads.push_back(std::thread([=]() {
@@ -138,29 +167,58 @@ void ThreadRunner::timeout(int ms, std::function<void()> callback) {
     }));
 }
 
+void ThreadRunner::join() {
+    while (threads.size() > 0) {
+        std::vector<std::thread> threadsT = std::move(threads);
+        for (std::thread &thread : threadsT) {
+            threadLog("Joining thread", thread.get_id());
+            thread.join();
+        }
+    }
+}
+
 void TasksDemo::run() {
     int i = 10;
 
-    rejectWith<int, const char *>("[Error 1]")(
-        [](int _) { threadLog("Never get here!"); },
-        [](auto err) { threadLog("Task rejected", err); }
+    ThreadRunner runner;
+
+    // resolveWith<const char *, int>(13);
+
+    // rejectWith<const char *, int>("[Error 1]")(
+    //     [](auto err) { threadLog("Task rejected", err); },
+    //     [](int _) { threadLog("Never get here!"); }
+    // );
+
+    // withDelay(globalRunner, 2000, rejectWith<const char *, int>("[Error 2]"))(
+    //     [](auto err) { threadLog("Task rejected", err); },
+    //     [](int _) { threadLog("Never get here!"); }
+    // );
+
+    // withRetry(globalRunner, 3000, rejectWith<const char *, int>("Error 3"), resolveWith<const char *, int>(42))(
+    //     [](auto _) { threadLog("Never get here!"); },
+    //     [=](int val) { threadLog("Task resolved", val, "with local variable i", i); }
+    // );
+
+    // withFinally<const char *, int>(
+    //     []() { threadLog("Finally block executed"); },
+    //     withRetry(globalRunner, 4000, rejectWith<const char *, int>("Error 3"), resolveWith<const char *, int>(100))
+    // )(
+    //     [](auto _) { threadLog("Never get here!"); },
+    //     [](int val) { threadLog("Task resolved", val); }
+    // );
+
+    auto task1 =
+        resolveWith<const char *, int>(123)
+            >> delay<const char *, int>(runner, 1000)
+            >> tap<const char *, int>([](int val) { threadLog("Hello 1"); })
+            >> delay<const char *, int>(runner, 1000)
+            >> tap<const char *, int>([](int val) { threadLog("Hello 2"); });
+
+    task1(
+        [](auto _) { threadLog("Never get here!"); },
+        [](int val) { threadLog("Task resolved", val); }
     );
 
-    withDelay(globalRunner, 2000, rejectWith<int, const char *>("[Error 2]"))(
-        [](int _) { threadLog("Never get here!"); },
-        [](auto err) { threadLog("Task rejected", err); }
-    );
-
-    withRetry(globalRunner, 3000, rejectWith<int, const char *>("Error 3"), resolveWith<int, const char *>(42))(
-        [=](int val) { threadLog("Task resolved", val, "with local variable i", i); },
-        [](auto _) { threadLog("Never get here!"); }
-    );
-
-    withFinally(
-        []() { threadLog("Finally block executed"); },
-        withRetry(globalRunner, 4000, rejectWith<int, const char *>("Error 3"), resolveWith<int, const char *>(100))
-    )(
-        [](int val) { threadLog("Task resolved", val); },
-        [](auto _) { threadLog("Never get here!"); }
-    );
+    runner.join();
+    threadLog("Exit demo");
 }
